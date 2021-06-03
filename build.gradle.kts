eb4742850e72b0500b2c65adf615ff68df19a438
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-06-02 22:34:46 -0700

    Use polyglot-version-catalogs plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
   id("java-gradle-plugin")
-  id("com.gradle.plugin-publish") version "0.13.0"
-  val indraVersion = "2.0.4"
-  id("net.kyori.indra") version indraVersion
-  id("net.kyori.indra.license-header") version indraVersion
-  id("net.kyori.indra.publishing.gradle-plugin") version indraVersion
-  id("net.kyori.indra.checkstyle") version indraVersion
+  id("com.gradle.plugin-publish")
+  id("net.kyori.indra")
+  id("net.kyori.indra.license-header")
+  id("net.kyori.indra.publishing.gradle-plugin")
+  id("net.kyori.indra.checkstyle")
 }
 
 group = "net.kyori"
@@ -18,12 +17,11 @@ repositories {
 }
 
 dependencies {
-  implementation("net.kyori", "mammoth", "1.0.0")
-  implementation("com.google.guava", "guava", "30.1.1-jre")
-  implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin-api", "1.4.31")
-  compileOnly("org.checkerframework", "checker-qual", "3.13.0")
-
-  checkstyle("ca.stellardrift", "stylecheck", "0.1")
+  implementation(libs.mammoth)
+  implementation(libs.guava)
+  implementation(libs.kotlinGradlePluginApi)
+  compileOnly(libs.checkerQual)
+  checkstyle(libs.stylecheck)
 }
 
 indra {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
+plugins {
+  id("ca.stellardrift.polyglot-version-catalogs") version "5.0.0"
+}
+
 rootProject.name = "blossom"
